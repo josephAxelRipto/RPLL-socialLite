37f0f82c1e30771071a7b4f68e2259f6a29c238d
sep150900 <79404091+sep150900@users.noreply.github.com>
2021-02-22 19:14:21 +0700

    create migration(member,post,bookmark,comment)

---
+++ Backend/SocialLite/database/migrations/2021_02_22_111653_create_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->string("username",20)->primary();
+            $table->string("fullname");
+            $table->date("birth");
+            $table->string("password");
+            $table->timestamps("memberJoinDate");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
---
+++ Backend/SocialLite/database/migrations/2021_02_22_112320_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->binary("image");
+            $table->date("datePost");
+            $table->integer("count_like");
+            $table->integer("countComment");
+            $table->string("caption");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
